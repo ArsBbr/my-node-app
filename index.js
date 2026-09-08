5d8bc01f570427458ec7d318c1881419be68561e
ars <arse2000ni@gmail.com>
2026-09-08 09:30:45 +0300

    Added student info and Pi calculation

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 const http = require('http');
+
 const server = http.createServer((req, res) => {
-res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
-res.end('<h1>ЏаЁўҐв, ¬Ёа!</h1>');
+    res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+    
+   
+    const fio = "Барбажинский Арсений Леонидович";
+    const group = "Группа 478";
+    
+        const piValue = (() => { 
+        let p = 0; 
+        for (let i = 0; i < 100; i++) p += (i % 2 ? -1 : 1) / (i * 2 + 1); 
+        return (p * 4).toFixed(1); 
+    })();
+
+    
+    res.end(`
+        <p>${fio}</p>
+        <p>${group}</p>
+        <p>${piValue}</p>
+    `);
 });
+
 const PORT = 3000;
 server.listen(PORT, () => {
-console.log(`‘ҐаўҐа § ЇгйҐ­ ­  http://localhost:${PORT}`);
+    console.log(`Сервер запущен на http://localhost:${PORT}`);
 });
